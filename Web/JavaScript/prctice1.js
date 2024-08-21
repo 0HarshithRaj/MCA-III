7c837b23c0eaf915be4210728e76c8b5b4fa8df9
0HarshithRaj <harshithnalkur5@gmail.com>
2024-08-21 14:56:05 +0530

    Added AIML and Web files

---
+++ Web/JavaScript/prctice1.js
@@ -0,0 +1,12 @@
+document.write("Hello </br>");
+var a = 123;
+var x = String(a, "</br>");
+var y = toString(a)
+document.write(typeof(a), "</br>");
+document.write(typeof(x), "</br>");
+document.write(typeof(y), "</br>");
+document.write(a,"</br>", x, "</br>");
+// document.write()
+alert("Hey thatnks for coming here!!!");
+confirm("Do you want to continue?");
+prompt("Enter your name here: ", "Raj");
